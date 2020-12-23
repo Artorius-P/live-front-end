17d897acb795cbbd2f32002559becd60173876e1
Artorius-P <dwxd@foxmail.com>
2020-12-23 11:07:47 +0800

    修改架构

--- src/components/Live.vue
+++ src/components/Live.vue
@@ -1,36 +1,11 @@
 <template>
   <div>
     <video id="videoElement" controls autoplay width="512" height="288"></video>
-    <el-footer style="text-align: center;">
-    <div v-if="isTeacher">
-      <el-select v-model="videoValue" placeholder="请选择视频源">
-        <el-option
-          v-for="item in videos"
-          :key="item"
-          :label="item"
-          :value="item">
-        </el-option>
-      </el-select>
-      <el-select v-model="audioValue" placeholder="请选择音频源">
-        <el-option
-          v-for="item in audios"
-          :key="item"
-          :label="item"
-          :value="item">
-        </el-option>
-      </el-select>
-    <el-button type="primary" @click="teacherPlay">开始推流</el-button>
-  </div>
-    </el-footer>
-    
-
   </div>
 </template>
 
 <script>
 import flvjs from "flv.js";
-
-
 
 export default {
   name: "list",
@@ -38,29 +13,19 @@ export default {
     return {
       flvPlayer: null,
       isTeacher: false,
-      videos: [],
-      videoValue: '',
-      audios: [],
-      audioValue: '',
       roomID: null,
     };
   },
   mounted() {
     this.roomID = this.$store.state.room.id;
-    if(this.$store.state.loginInfo.identity == '1') {
-      this.isTeacher =true
-      this.$axios.get('http://127.0.0.1:8086/device').then((response) => {
-      this.videos = response.data.video
-      this.audios = response.data.audio
-    }).catch((error) => {
-          this.$message.error(error)
-        })
+    if (this.$store.state.loginInfo.identity == "1") {
+      this.isTeacher = true;
     }
     if (flvjs.isSupported()) {
-      let liveUrl =
-        "http://a.boynextdoor.top/srs/live/" +
-        this.roomID +
-        ".flv";
+      let liveUrl = "http://a.boynextdoor.top/srs/live/" + this.roomID + ".flv";
+      if (this.isTeacher) {
+        liveUrl = "http://a.boynextdoor.top/stu" + this.roomID + ".flv";
+      }
       var videoElement = document.getElementById("videoElement");
       this.flvPlayer = flvjs.createPlayer({
         type: "flv",
@@ -73,17 +38,22 @@ export default {
   methods: {
     play() {
       this.flvPlayer.play();
-      
     },
-    teacherPlay () {
-      this.$axios.post('http://127.0.0.1:8086/play', {
-            audio: this.audioValue,
-            video: this.videoValue,
-            url: 'rtmp://a.boynextdoor.top:1935/live/'+ this.roomID
-          }).then((res) => {
-            console.log(res.data)
-          })
+    teacherPlay() {
+      let videoUrl = "rtmp://a.boynextdoor.top:1935/live/" + this.roomID
+      if (!this.isTeacher) {
+        videoUrl = "rtmp://a.boynextdoor.top:1935/live/stu" + this.roomID
       }
+      this.$axios
+        .post("http://127.0.0.1:8086/play", {
+          audio: this.audioValue,
+          video: this.videoValue,
+          url: videoUrl,
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
   },
 };
 </script>
---
+++ src/components/Teacher.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <div v-if="this.$store.state.loginInfo.isLoggedIn">
+      <el-header>
+        <h2 v-if="isConnected">学生画面已开启</h2>
+      </el-header>
+      <el-main>
+        <live v-if="isConnected"></live>
+        <el-row>
+          <br />
+        </el-row>
+        <el-row>
+          <el-button
+            @click="whiteBoard = true"
+            icon="el-icon-edit"
+            style="margin-left: 16px"
+            plain
+          >
+            白板
+          </el-button>
+          <el-button
+            @click="showchat"
+            icon="el-icon-chat-round"
+            style="margin-left: 16px"
+            plain
+          >
+            聊天
+          </el-button>
+          <el-button
+            @click="talk"
+            icon="el-icon-chat-round"
+            style="margin-left: 16px"
+            plain
+          >
+            连麦
+          </el-button>
+        </el-row>
+        <el-drawer
+          title="聊天室"
+          :visible.sync="this.$store.state.chat"
+          direction="rtl"
+          :before-close="chatClose"
+        >
+          <chatPage></chatPage>
+        </el-drawer>
+
+        <el-drawer
+          title="白板"
+          :visible.sync="whiteBoard"
+          direction="ttb"
+          size="100%"
+        >
+          <boardPage></boardPage>
+        </el-drawer>
+      </el-main>
+      <el-footer style="text-align: center">
+        <div>
+          <el-select v-model="videoValue" placeholder="请选择视频源">
+            <el-option
+              v-for="item in videos"
+              :key="item"
+              :label="item"
+              :value="item"
+            >
+            </el-option>
+          </el-select>
+          <el-select v-model="audioValue" placeholder="请选择音频源">
+            <el-option
+              v-for="item in audios"
+              :key="item"
+              :label="item"
+              :value="item"
+            >
+            </el-option>
+          </el-select>
+          <el-button type="primary" @click="teacherPlay">开始推流</el-button>
+        </div>
+      </el-footer>
+    </div>
+
+    <div v-if="!this.$store.state.loginInfo.isLoggedIn">
+      <notLogin></notLogin>
+    </div>
+  </div>
+</template>
+
+<script>
+import notLogin from "@/components/NotLogin";
+import live from "@/components/Live";
+import chatPage from "@/components/Chat";
+import boardPage from "@/components/WhiteBoard";
+
+export default {
+  data() {
+    return {
+      whiteBoard: false,
+      roomId: null,
+      img: null,
+      isConnected: false,
+      videos: [],
+      videoValue: "",
+      audios: [],
+      audioValue: "",
+    };
+  },
+  mounted() {
+     this.$axios
+          .get("http://127.0.0.1:8086/device")
+          .then((response) => {
+            this.videos = response.data.video;
+            this.audios = response.data.audio;
+          })
+          .catch((error) => {
+            this.$message.error(error);
+          });
+    this.roomId = "onlineEduStu" + this.$store.state.room.id;
+  },
+  components: {
+    notLogin,
+    live,
+    chatPage,
+    boardPage,
+  },
+  methods: {
+    showchat() {
+      this.$store.state.chat = true;
+    },
+    chatClose() {
+      this.$store.state.chat = false;
+    },
+    talk() {
+      if (!this.isConnected) {
+        this.isConnected = true;
+      } else {
+        this.isConnected = false;
+      }
+    },
+    logEvent(event) {
+      console.log("Event : ", event);
+    },
+  },
+  computed: {},
+};
+</script>
